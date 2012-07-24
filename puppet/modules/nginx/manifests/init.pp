8d301eee232b9ecfbe68c400896637e9bd3ebf8f
Jorge Bastida <me@jorgebastida.com>
2012-07-24 19:17:09 +0200

    Add warehouse

--- puppet/modules/nginx/manifests/init.pp
+++ puppet/modules/nginx/manifests/init.pp
@@ -13,6 +13,15 @@ class nginx {
     notify  => Service['nginx']
   }
 
+  file { "/etc/nginx/sites-enabled/warehouse.conf":
+    owner   => root,
+    group   => root,
+    mode    => 644,
+    source  => 'puppet:///modules/nginx/warehouse.conf',
+    require => Package['nginx'],
+    notify  => Service['nginx']
+  }
+
   service { 'nginx':
     ensure      => running,
     enable      => true,
