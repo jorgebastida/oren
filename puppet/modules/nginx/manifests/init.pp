bddee8826de2fc6754713220dcae2c8a2a0ebc38
Jorge Bastida <me@jorgebastida.com>
2012-08-09 22:34:32 +0100

    Remove router conf

--- puppet/modules/nginx/manifests/init.pp
+++ puppet/modules/nginx/manifests/init.pp
@@ -22,15 +22,6 @@ class nginx {
     notify  => Service['nginx']
   }
 
-  file { "/etc/nginx/sites-enabled/router.conf":
-    owner   => root,
-    group   => root,
-    mode    => 644,
-    source  => 'puppet:///modules/nginx/router.conf',
-    require => Package['nginx'],
-    notify  => Service['nginx']
-  }
-
   service { 'nginx':
     ensure      => running,
     enable      => true,
